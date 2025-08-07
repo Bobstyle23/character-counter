adb582b0a55eb12ef05e4ce3a31570c1c3db0f1e
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-08-07 17:53:45 +0900

    Add logic to count chars, words, and sentences

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const charLimitCheckbox = document.getElementById("limit");
 const charLimitInputbox = document.getElementById("character-limit");
-
+const textToAnalyze = document.querySelector(".main__text");
 const filterForm = document.querySelector(".main__filters");
+
+const characterCount = document.querySelector(".result__count--chars");
+const wordCount = document.querySelector(".result__count--words");
+const sentenceCount = document.querySelector(".result__count--sentences");
 
 charLimitCheckbox.addEventListener("change", function (event) {
   const isChecked = event.target.checked;
@@ -12,4 +16,24 @@ charLimitCheckbox.addEventListener("change", function (event) {
 
 filterForm.addEventListener("submit", function (e) {
   e.preventDefault();
+});
+
+const totalCounts = {
+  totalChars: null,
+  totalWords: null,
+  totalSentences: null,
+};
+
+textToAnalyze.addEventListener("input", function (e) {
+  const value = e.target.value;
+  let { totalChars, totalWords, totalSentences } = totalCounts;
+  totalChars = value.split("");
+  totalWords = value.split(" ").filter((text) => Boolean(text));
+  totalSentences = value.split(".").filter((text) => Boolean(text));
+
+  characterCount.textContent = totalChars.length.toString().padStart("2", "0");
+  wordCount.textContent = totalWords.length.toString().padStart("2", "0");
+  sentenceCount.textContent = totalSentences.length
+    .toString()
+    .padStart("2", "0");
 });
